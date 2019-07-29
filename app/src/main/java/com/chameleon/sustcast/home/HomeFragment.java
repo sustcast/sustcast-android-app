b73df50eb829f878eb01ccdf4d5763d5e403bdd0
appledora <immobile.imbecile0010@gmail.com>
2019-07-30 03:45:34 +0600

    home fragment clear

--- app/src/main/java/com/chameleon/sustcast/home/HomeFragment.java
+++ app/src/main/java/com/chameleon/sustcast/home/HomeFragment.java
@@ -28,7 +28,6 @@ import retrofit2.Response;
 public class HomeFragment extends Fragment {
     private static final String TAG = "Home Fragment";
     Button b_play;
-    private CurrentClient mAPIService;
 
 
     MediaPlayer mediaPlayer;
@@ -42,50 +41,11 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        mAPIService = ApiUtils.getMetadataService();
         mediaPlayer = new MediaPlayer();
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         new HomeFragment.PlayerTask().execute(stream);
-       catchMetadata();
         return view;
     }
-
-
-    public void catchMetadata(){
-        mAPIService.fetch().enqueue(new Callback<OuterCurrent>() {
-            @Override
-            public void onResponse(Call<OuterCurrent> call, Response<OuterCurrent> response) {
-                System.out.println("Response code =>" + response.code());
-                //System.out.println("JSON => " + new GsonBuilder().setPrettyPrinting().create().toJson(response));
-                if (response.isSuccessful()) {
-                    Log.i("MY", "Response Metadata successful");
-                    List<Current> currentList = response.body().getCurrent();
-                    String artist = currentList.get(0).getArtist();
-                    String songtitle = currentList.get(0).getSong();
-                    String genre = currentList.get(0).getGenre();
-                    String lyric = currentList.get(0).getLyric();
-                    Log.i("MY", "ARTIST here => "+ currentList.get(0).getLyric());
-
-                } else {
-                    Log.i("MY", "Response Metadata NOT successful");
-                    System.out.println("JSON => " +response.body());
-                    System.out.println("RESPONSE: " + response.toString());
-
-                }
-
-
-            }
-
-            @Override
-            public void onFailure(Call<OuterCurrent> call, Throwable t) {
-                Log.i("MY", "Response Metadata FAILED");
-
-            }
-        });
-
-    }
-
-
 
 
     class PlayerTask extends AsyncTask<String, Void, Boolean> {
